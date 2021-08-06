2556ba2072f1ae942ed977d442edd5a2c452833c
GustavoHLC <gutohenri_car@hotmail.com>
2021-08-06 12:57:35 -0300

    implementação de get, post, put e delete na postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
     return this.http.post<User>('https://blogpessoalguugs.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id:number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoalguugs.herokuapp.com/usuarios/${id}`)
+  }
+
   logado() {
     let ok: boolean = false
     if(environment.token != '') {
